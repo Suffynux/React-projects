43c92f87f627f8955cf137570abfd9244adcf6e3
Sufiyan <suffynux@gmail.com>
2025-03-08 14:44:01 +0500

    First assigment completed

--- React-Basics/src/App.jsx
+++ React-Basics/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
-import Chai from './chai'
+import { useState } from "react";
+import Chai from "./chai";
 function App() {
-  const username = "Sachin";
+  let [counter, setCounter] = useState(0);
+  const incrementValue = () => {
+    if (counter >= 20) return;
+    setCounter(counter + 1);
+  };
+
+  const decrementValue = () => {
+    if (counter < 0) return;
+    setCounter(counter - 1);
+  };
+
   return (
     <>
-    <h1>username is {username}</h1>
+      <h1>Counter Value {counter}</h1>
+      <button onClick={incrementValue}>Increment</button>
+      <button onClick={decrementValue}>Decrement</button>
+      <p>{counter}</p>
+      <h1>{counter}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
