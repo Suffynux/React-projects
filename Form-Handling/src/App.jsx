123c1053a039ed88d82f9215c8cbb0a9aaa9c6ce
Sufiyan <suffynux@gmail.com>
2025-05-28 16:34:50 +0500

    Form all states added

---
+++ Form-Handling/src/App.jsx
@@ -0,0 +1,169 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleForm = (event) => {
+    event.preventDefault();
+    console.log({ name, phone, email, password, remember });
+  };
+
+  return (
+    <>
+      <h1 className="text-center bg-red-200 p-5 font-bold text-xl">
+        Form Handling
+      </h1>
+      <form onSubmit={handleForm} className="max-w-sm mx-auto mt-5">
+        {/* Name */}
+        <div className="mb-5">
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+              focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+              dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
+              dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="John Doe"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            rows="3"
+          />
+        </div>
+
+        {/* Phone */}
+        <div className="mb-5">
+          <label
+            htmlFor="phone"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your phone
+          </label>
+          <input
+            type="tel"
+            id="phone"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+              focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+              dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
+              dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="03XXXXXXXXX"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            required
+          />
+        </div>
+
+        {/* Email */}
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+              focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+              dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
+              dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="name@domain.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        {/* Password */}
+        <div className="mb-5">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+              focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+              dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
+              dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        {/* Message */}
+        <div className="mb-5">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your password
+          </label>
+          <textarea
+            id="name"
+            rows={3}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+    focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+    dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
+    dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Message"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          ></textarea>
+        </div>
+
+        {/* Remember Me */}
+        <div className="flex items-start mb-5">
+          <div className="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              checked={remember}
+              onChange={() => setRemember(!remember)}
+              className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 
+                focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 
+                dark:border-gray-600 dark:focus:ring-blue-600 
+                dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+            />
+          </div>
+          <label
+            htmlFor="remember"
+            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Remember me
+          </label>
+        </div>
+
+        {/* Submit Button */}
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
+            focus:outline-none focus:ring-blue-300 font-medium rounded-lg 
+            text-sm w-full sm:w-auto px-5 py-2.5 text-center 
+            dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default App;
